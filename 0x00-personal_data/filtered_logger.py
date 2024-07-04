b7e3ec09cca096f2c60b5932075a30abb4023098
AnasJafir <dr.anas.jafir@gmail.com>
2024-07-04 22:43:46 +0000

    Filtring values in incoming log records

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,6 +2,7 @@
 """Obfuscating Module"""
 import re
 from typing import List
+import logging
 
 
 def filter_datum(
@@ -28,3 +29,27 @@ def filter_datum(
                 message
                 )
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        Filters values in incoming log records using filter_datum
+        """
+        return filter_datum(
+                self.fields,
+                self.REDACTION,
+                super().format(record),
+                self.SEPARATOR
+                )
